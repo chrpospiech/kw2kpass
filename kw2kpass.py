540fcf6f6b46ff40f98e042b8d29e7f87cbbc620
Dr. Christoph Pospiech <pospiech-HD@t-online.de>
2026-06-04 18:22:43 +0200

    Resurrect copy_wallet_folder.

--- kw2kpass.py
+++ kw2kpass.py
@@ -15,10 +15,11 @@
 import logging
 import re
 
-from cli import getOptionsAndDefaults
+from cli import get_options_and_defaults
+from keepass import ProtectedString
 from keepass_utils import close_database, open_database
 from kwallet import WalletIterator
-from wallet_utils import OpenWallet, SetWalletFolder
+from wallet_utils import find_or_create_entry, open_wallet, set_wallet_folder
 
 logging.basicConfig(
     level=logging.WARNING,
@@ -27,60 +28,35 @@ logging.basicConfig(
 logger = logging.getLogger("kw2kpass")
 
 
-# def CopyWalletFolder(wallet, Wfolder, Wfilter, psafe, pgroup):
-#     """
-#     @summary: Copy filtered entries from Wname:Wfolder
-#                       into psafe:pgroup
-#     @param wallet: Kwallet handle
-#     @param Wfolder: Kwallet folder
-#     @param Wfilter: Filter to filter entries
-#     @param psafe: Password Safe handle
-#     @param pgroup: Password Safe group
-#     """
-#     if wallet.hasFolder(Wfolder):
-#         logger.info("folder {0} found".format(Wfolder))
-#     else:
-#         print("No folder {0} found".format(Wfolder))
-#         sys.exit(2)
-#     if wallet.setFolder(Wfolder):
-#        logger.info("folder {0} set".format(Wfolder))
-#    else:
-#        print("No folder {0} could not be set".format(Wfolder))
-#        sys.exit(2)
-# [...]
-# newRecord = PWSrecordHandle()
-# newRecord.Clear()
-# newRecord.SetTitle(title)
-# newRecord.SetGroup(pgroup)
-# newRecord.SetURL(host)
-# newRecord.SetUser(uid)
-# newRecord.SetEmail(email)
-# newRecord.SetPassword(passwd)
-# newRecord.CreateUUID()
-# psafe.WriteRecord(newRecord)
+def copy_wallet_folder(wallet, Wfolder, Wfilter, dbase, group):
+    """Copy filtered entries from Wname:Wfolder into KeePass group.
 
-
-def PrintWalletFolder(wallet, Wfolder, Wfilter):
-    """Print filtered entries from Wname:Wfolder.
-
-    @summary: Print filtered entries from Wname:Wfolder
     @param wallet: Kwallet handle
     @param Wfolder: Kwallet folder
-    @param Wfilter: Filter to filter entries.
+    @param Wfilter: Filter to filter entries
+    @param dbase: KeePassXC database handle
+    @param group: KeePassXC group
     """
-    SetWalletFolder(wallet, Wfolder)
+    set_wallet_folder(wallet, Wfolder)
     for e in WalletIterator(wallet):
-        uidFilter = re.compile(r"\@")
         title = str(e)
         uid = wallet.username(e)
         host = wallet.hostname(e)
         passwd = wallet.password(e)
+        # Skip the entry that lists the URLs without password saving
+        if passwd == "n/a":
+            logger.debug(f"entry {title}  has no password")
+            continue
+        # Skip the entry if it does not match the filter
+        if not Wfilter.search(title):
+            logger.info(f"entry {title} has been filtered out")
+            continue
         """
 		Entries in Kwallet can be password or map entries;
 		password entries return empty username and hostname.
 		"""
         if not str(uid):
-            logger.debug("entry " + title + " is an ordinary password entry")
+            logger.debug(f"entry {title} is an ordinary password entry")
             titleParts = re.split(r"\@", title)
             if len(titleParts) > 1:
                 uid = titleParts[0]
@@ -89,42 +65,25 @@ def PrintWalletFolder(wallet, Wfolder, Wfilter):
                 uid = ""
                 host = titleParts[0]
         else:
-            logger.debug("entry " + title + " is a map entry")
-        # Skip the entry that lists the URLs without
-        # password saving
-        if passwd == "n/a":
-            logger.debug("entry " + title + "  has no password")
-            continue
-        logger.debug("On host " + str(host) + ":")
-        logger.debug("u = " + str(uid) + " pw = " + str(passwd))
-        if uidFilter.search(str(uid)):
-            logger.debug("uid " + str(uid) + " is an email address")
-            str(uid)
-        else:
-            logger.debug("uid " + str(uid) + " is not an email address")
-        if not Wfilter.search(str(uid) + str(host)):
-            logger.info("host " + str(host) + " has been filtered out")
-            continue
-        logger.info("Printing record for " + title)
-        print("On host " + str(host) + ":")
-        print("u = " + str(uid) + " pw = " + str(passwd))
+            logger.debug(f"entry {title} is a map entry")
+        logger.info(f"Copying record for {title}")
+        logger.debug(f"On host {host}:")
+        logger.debug(f"u = {uid} pw = {passwd}")
+        entry = find_or_create_entry(dbase, group, title)
+        entry.set_url(ProtectedString(str(host)))
+        entry.set_username(ProtectedString(str(uid)))
+        entry.set_password(ProtectedString(str(passwd)))
 
 
 def main():
-    """Main loop of extracting from Kwallet and adding to KeePassXC database.
-
-    Output is temporarily written to stdout for debugging purposes.
-    The final version will write directly to the database.
-    """
-    Wname, Wfilter, WsafeMap, Kinfile, Koutfile, Kpasswd = getOptionsAndDefaults()
+    """Main loop of extracting from Kwallet and adding to KeePassXC database."""
+    Wname, Wfilter, WsafeMap, Kinfile, Koutfile, Kpasswd = get_options_and_defaults()
     database = open_database(Kinfile, Kpasswd)
-    wallet = OpenWallet(Wname)
-    # for Wfolder in WsafeMap:
-    # pgroup = WsafeMap[Wfolder]
-    # CopyWalletFolder(wallet, Wfolder, Wfilter, psafe, pgroup)
-    close_database(database, Koutfile, Kpasswd)
+    wallet = open_wallet(Wname)
     for Wfolder in WsafeMap:
-        PrintWalletFolder(wallet, Wfolder, Wfilter)
+        group = WsafeMap[Wfolder]
+        copy_wallet_folder(wallet, Wfolder, Wfilter, database, group)
+    close_database(database, Koutfile, Kpasswd)
 
 
 if __name__ == "__main__":
