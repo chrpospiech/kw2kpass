31efd599ad6785ad287cde5255601b68615e4230
Dr. Christoph Pospiech <pospiech-HD@t-online.de>
2026-06-17 09:53:06 +0200

    Revert "Add another ... in the pyi file." ruff disagrees with AI.
    This reverts commit 489eee379d2fcca86b2ccc9421d166b7df5a315f.

--- test_wallet.pyi
+++ test_wallet.pyi
@@ -61,4 +61,3 @@ class TestReadWallet:
         Calls kwalletd5's deleteWallet and removes the .kwl / .salt
         files that initTestCase placed in the kwalletd directory.
         """
-        ...
